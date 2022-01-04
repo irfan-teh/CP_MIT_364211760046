39cfeddee43989b070db06702652fe860e57c625
irfan-teh <irfan.t@rmutsvmail.com>
2022-01-04 12:09:35 +0700

    Test upload to github

---
+++ Leb1/ex_variable.py
@@ -0,0 +1,51 @@
+# Comment one line
+
+# Name: Irfan teh
+# SID: 364211760046
+# Group: MIT212
+
+
+# Comment multiple lines
+
+"""
+Python programming is the best !!!
+Python is easy !!!
+Python is similer to English language !!!
+Hello World campus
+"""
+
+# Print() command
+print('Hello MIT212')
+
+# variable -> ตัวเเปร
+x = 10 # int
+print(x)
+# type of variable -> ชนิดของข้อมูล
+print(type(x))
+
+x = 'RUTS' # str --> String ข้อความ
+print(x)
+print(type(x))
+
+x = 12.12 # floating --> จำนวนจริง
+print(x)
+print(type(x))
+
+x = True # bool --> Ture,false
+print(x)
+print(type(x))
+
+# x = 100
+# y =200
+x, y = 100, 200 # assign value to variables
+
+# print valus and type of x
+print('valus of x is', x, 'and type of x is',type(x))
+# print valus and type of y
+print('valus of y is', y, 'and type of x is',type(y))
+# print summation of x and y (เเเสดงผลลัพธ์ผลรวมของตัวเเปร x เเละ y)
+print('x+y=', x+y)
+print('summation of x and y is', x+y)
+print('Using sum() x + y =' , sum((x, y)))
+z = x+y
+print(z)
